932a2087960e51699e92515eac599ff409b6ba9e
Luiz Fernando Alvarenga <luizfca7@gmail.com>
2022-02-22 12:55:18 -0300

    TAKS CRUD MISSING CHECKBOX

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #criar novas tasks
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  # mostrar todas as tasks na pagina tasks
+  get '/tasks', to: 'tasks#index', as: :index
+
+  # mostrar detalhes de uma tsk especifica
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # atualizar uma task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # deletar uma task
+  delete "tasks/:id", to: "tasks#destroy", as: :destroy
+
+
 end
