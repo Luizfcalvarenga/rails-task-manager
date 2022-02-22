41c41f70a490e3d0a5cf90c63745c1d13cf898ce
Luiz Fernando Alvarenga <luizfca7@gmail.com>
2022-02-22 16:07:40 -0300

    TASK MANAGER CRUD RAILS

--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,17 @@ Rails.application.routes.draw do
   get 'tasks/new', to: 'tasks#new'
   post 'tasks', to: 'tasks#create'
 
-  # mostrar todas as tasks na pagina tasks
+  # # mostrar todas as tasks na pagina tasks
   get '/tasks', to: 'tasks#index', as: :index
 
-  # mostrar detalhes de uma tsk especifica
+  # # mostrar detalhes de uma tsk especifica
   get 'tasks/:id', to: 'tasks#show', as: :task
 
-  # atualizar uma task
+  # # atualizar uma task
   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
   patch 'tasks/:id', to: 'tasks#update'
 
-  # deletar uma task
+  # # deletar uma task
   delete "tasks/:id", to: "tasks#destroy", as: :destroy
-
-
+  # resources :tasks
 end
